7011f1105a2451700f9b2736f687bd9abc9a3e46
cddevs <cbcdds@gmail.com>
2021-08-08 18:43:03 +1000

    Completed "check_input_number"

--- ATR Max Area Calculator.py
+++ ATR Max Area Calculator.py
@@ -1,4 +1,52 @@
 import math
+
+def another():
+    b = input("Would you like to do another? (\"y\" for yes, \"n\" for no) ")
+    if b == "n":
+        print("Goodbye")
+        return False
+
+def check_input_number(input):
+    try:
+        # Convert it into float
+        float(input)
+        return True
+    except ValueError:
+        return False
+
+
+def coverage():
+    while True:
+        user_input = input("Enter the Height of ATR7000 from the Ground: ")
+        if check_input_number(user_input):
+            print("Valid input.")
+            height = float(user_input)**2
+            area = math.sqrt(3)/2 * 4 * math.tan(math.radians(60))**2 * height
+            radius = math.sqrt(area/math.pi)
+            diamter = radius * 2
+            print()
+            print(f"Max ATR Coverage Area = {area:.2f}")
+            print(f"Radius at Ground Level = {radius:.2f}")
+            print(f"Diameter at Ground Level = {diamter:.2f}")
+            break
+        else:
+            print("Error: Invalid input.")
+
+
+ 
+
+
+#Main Program Code
+while True:
+    print("\n==============================================================")
+    coverage()
+    if not another():
+        break
+
+   
+
+"""
+import math
 import time
 a = 1
 while a > 0:
@@ -20,3 +68,5 @@ while a > 0:
         print("Goodbye")
         time.sleep(1)
         a = 0
+
+"""

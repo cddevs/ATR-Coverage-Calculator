c77e786d4a93c069981435eebcd3b0ebbd716170
cddevs <cbcdds@gmail.com>
2021-08-09 11:46:20 +1000

    New version built.

--- ATR Max Area Calculator.py
+++ ATR Max Area Calculator.py
@@ -1,72 +1,57 @@
 import math
 
 def another():
-    b = input("Would you like to do another? (\"y\" for yes, \"n\" for no) ")
-    if b == "n":
-        print("Goodbye")
-        return False
+    while True:
+        user_input = input("\nWould you like complete another coverage calculation? (\"y\" or \"n\") ")
+        if user_input.isalpha():
+            if user_input.lower() == "y":
+                return True
+            elif user_input.lower() == "n":            
+                print("\nThank you and goodbye!")
+                return False
+            else:
+                print("\nError: Invalid input.")
+        else:
+            print("\nError: Invalid input.")
+
+
+def calculate_coverage_area(height, scan_angle, scan_angle_use):
+    if check_input_number(scan_angle):  
+        height = float(height)
+        area = (math.sqrt(3)/2) * (4*(math.tan(math.radians(scan_angle))**2)) * (height**2)
+        radius = math.sqrt(area/math.pi)
+        diamter = radius * 2
+        print(f"\nAt a height of {height:.2f} and a {scan_angle_use} elevation scan angle of {scan_angle:.2f} degrees:")
+        print(f"\n\tMax ATR Coverage Area = {area:.2f}")
+        print(f"\tRadius at Ground Level = {radius:.2f}")
+        print(f"\tDiameter at Ground Level = {diamter:.2f}")
+    else:
+        print("\nError: Invalid scan angle.")
+
 
 def check_input_number(input):
     try:
         # Convert it into float
-        float(input)
-        return True
+        if (float(input) > 0):
+            return True
     except ValueError:
         return False
 
 
 def coverage():
     while True:
-        user_input = input("Enter the Height of ATR7000 from the Ground: ")
+        user_input = input("\nEnter the Height of ATR7000 from the Ground: ")
         if check_input_number(user_input):
-            print("Valid input.")
-            height = float(user_input)**2
-            area = math.sqrt(3)/2 * 4 * math.tan(math.radians(60))**2 * height
-            radius = math.sqrt(area/math.pi)
-            diamter = radius * 2
-            print()
-            print(f"Max ATR Coverage Area = {area:.2f}")
-            print(f"Radius at Ground Level = {radius:.2f}")
-            print(f"Diameter at Ground Level = {diamter:.2f}")
+            calculate_coverage_area(user_input, 60, "default")
+            calculate_coverage_area(user_input, 54.7, "location accuracy")
             break
         else:
-            print("Error: Invalid input.")
-
-
- 
+            print("\nError: Invalid height input.")
 
 
 #Main Program Code
 while True:
     print("\n==============================================================")
     coverage()
-    if not another():
+    if another() == False :
         break
-
-   
-
-"""
-import math
-import time
-a = 1
-while a > 0:
-    print()
-    print('==============================================================')
-    h = float(input('Enter the Height of ATR7000 from the Ground: '))**2
-    area = math.sqrt(3)/2 * 4 * math.tan(math.radians(60))**2 * h
-    #area = 10.4 * h
-    r = math.sqrt(area/math.pi)
-    d = r * 2
-    print()
-    print('Max ATR Coverage Area =',(area:.2f))
-    print('Radius at Ground Level =',r)
-    print('Diameter at Ground Level =',d)
-    print()
-    print()
-    b = input("Would you like to do another? ('y' for yes, 'n' for no) ")
-    if b == 'n':
-        print("Goodbye")
-        time.sleep(1)
-        a = 0
-
-"""

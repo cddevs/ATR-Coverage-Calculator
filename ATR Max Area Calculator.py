9e88baa1795fb08c5fc0151720cc72b3c7ad5ed1
cddevs <cbcdds@gmail.com>
2021-08-09 16:32:00 +1000

    Updated output wording.

--- ATR Max Area Calculator.py
+++ ATR Max Area Calculator.py
@@ -18,10 +18,10 @@ def calculate_coverage_area(height, scan_angle, scan_angle_use):
         area = (math.sqrt(3)/2) * (4*(math.tan(math.radians(scan_angle))**2)) * (height**2)
         radius = math.sqrt(area/math.pi)
         diamter = radius * 2
-        print(f"\nAt a height of {height:.2f} and a {scan_angle_use} elevation scan angle of {scan_angle:.2f} degrees:")
+        print(f"\nAt a height of {height:.2f} and a {scan_angle_use} elevation scan angle of {scan_angle:.2f} degrees, at ground level the:")
         print(f"\n\tMax ATR Coverage Area = {area:.2f}")
-        print(f"\tRadius at Ground Level = {radius:.2f}")
-        print(f"\tDiameter at Ground Level = {diamter:.2f}")
+        print(f"\tRadius of the Coverage Area = {radius:.2f}")
+        print(f"\tDiameter of the Coverage Area = {diamter:.2f}")
     else:
         print("\nError: Invalid scan angle.")
 
@@ -39,7 +39,7 @@ def coverage():
         user_input = input("\nEnter the Height of ATR7000 from the Ground: ")
         if check_input_number(user_input):
             calculate_coverage_area(user_input, 60, "default")
-            calculate_coverage_area(user_input, 54.7, "location accuracy")
+            calculate_coverage_area(user_input, 54.7, "recommended location accuracy")
             break
         else:
             print("\nError: Invalid height input.")
